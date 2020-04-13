e173b2fc72d3d97b46a69b63143a5a85ba5409b8
huly94 <ico.giulianelli@hotmail.it>
2020-04-13 12:22:11 +0200

    Sensor Presence added

---
+++ smart_proj/Sensors/SensorPresence.py
@@ -0,0 +1,33 @@
+import logging
+
+import statemachine
+
+import smart_proj.Sensors.Sensor
+
+
+# A simple Occupancy Sensor (probably a Passive InfraRed sensor)
+# that detects if there is at least 1 visitor in the area or none.
+# It cannot differentiate between different visitors and cannot count how many visitors are in the room.
+#
+# We assume that the sensor internally includes a timer.
+# If no motion event is received after a given period of time, the sensor assumes that
+# the room is no longer occupied by a visitor.
+# When the sensor receives consecutive motion detection events, it will suppress them.
+class SensorPresence(smart_proj.Sensors.Sensor.Sensor):
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    empty = statemachine.State("Not detected", initial= True)
+    non_empty = statemachine.State("Person detected")
+
+    visitor_arrived = empty.to(non_empty)
+    visitor_left = non_empty.to(empty)
+
+    def __init__(self):
+        super().__init__()
+
+    def on_enter_non_empty(self):
+        logging.info("[SENSOR PRESENCE]: A new visitor has arrived, new state:" + self.current_state.name)
+        self.notify()
+
+    def on_enter_empty(self):
+        logging.info("[SENSOR PRESENCE]: Visitor has left, new state:" + self.current_state.name)
+        self.notify()
